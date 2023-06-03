a402ce213af2de22e91023c31adedcbc99f0e1be
巻嶋穹 <makishima@makishimanoMacBook-Pro.local>
2023-06-03 11:56:35 +0900

    push restapi

---
+++ migrate/migrate.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"go_restapi/db"
+	"go_restapi/model"
+)
+
+func main() {
+	dbConn := db.NewDB()
+	defer fmt.Println("Successfully Migrated")
+	defer db.CloseDB(dbConn)
+	dbConn.AutoMigrate(&model.User{}, &model.Task{})
+}
